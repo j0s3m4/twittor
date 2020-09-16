3af7d6b15f90dce0a42d84cdc2a27cb703a7dfed
jose-romero <j0s3r0m3r0>
2020-09-16 22:40:33 +0200

    Modificaciones para ios

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 // imports
 importScripts('js/sw-utils.js');
 
-
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -28,8 +27,6 @@ const APP_SHELL_INMUTABLE = [
     'js/libs/jquery.js'
 ];
 
-
-
 self.addEventListener('install', e => {
 
     const cacheStatic = caches.open( STATIC_CACHE ).then( cache => {
@@ -39,7 +36,6 @@ self.addEventListener('install', e => {
     const cacheInmutable = caches.open( INMUTABLE_CACHE ).then( cache => {
         cache.addAll( APP_SHELL_INMUTABLE );
     });
-
 
     e.waitUntil( Promise.all([cacheStatic, cacheInmutable]) );
 
